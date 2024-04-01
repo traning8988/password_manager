5fbc9a6ab01b33816dc8048798060f003ebfaf21
Shota Nakamichi <s02178906@gmail.com>
2024-04-01 12:54:28 +0900

    password_manager.shに追記

--- password_manager.sh
+++ password_manager.sh
@@ -7,14 +7,19 @@ do
     "Add Password")read -p "サービス名を入力してください:" ServiceName
      	           read -p "ユーザー名を入力してください:" UserName
 	           read -p "パスワードを入力してください:" Password
-	           echo "$ServiceName:$UserName:$Password" >> password.txt;;
+	           echo "$ServiceName:$UserName:$Password" >> password.txt
+		   echo "パスワードの追加は成功しました。";;
     "Get Password")read -p "サービス名を入力してください:" Service
 		   RESULT=$(grep -E "^$Service:" password.txt)
-		   IFS=":"
-                   set -- $RESULT
-		   echo "サービス名：$1"
-		   echo "ユーザー名：$2"
-		   echo "パスワード：$3";;
+		   if [ -n "$RESULT" ]; then
+		     IFS=":"
+                     set -- $RESULT
+		     echo "サービス名：$1"
+		     echo "ユーザー名：$2"
+		     echo "パスワード：$3"
+		   else
+		     echo "そのサービスは登録されていません。"
+		   fi;;
     Exit)echo -e "Thank you\033[31m!\033[m"
          exit 0;;
     *)echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。";;
